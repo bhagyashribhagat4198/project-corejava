54081e6b7fd3cc6c40f05c8fdb1b0d30295d33b3
Bhagyashri Bhagat <113599237+bhagyashribhagat4198@users.noreply.github.com>
2023-06-13 17:23:54 +0530

    Add files via upload

---
+++ testprotected.java
@@ -0,0 +1,11 @@
+package abstraction;
+
+import accessmodifiers.protectedexample;
+
+public class testprotected extends protectedexample {
+
+	public static void main(String[] args) {
+		testprotected obj =new testprotected();
+		obj.show();
+	}
+}
